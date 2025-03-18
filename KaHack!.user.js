9a210b92fe7e9cc98f0ff02bc180069aefde5a38
John Wee <156491565+johnweeky@users.noreply.github.com>
2025-03-18 23:18:03 +0800

    Update KaHack!.user.js

--- KaHack!.user.js
+++ KaHack!.user.js
@@ -1,11 +1,11 @@
 // ==UserScript==
 // @name         KaHack!
 // @version      1.0.25
-// @namespace    https://github.com/jokeri2222
 // @description  A hack for kahoot.it!
-// @updateURL    https://github.com/jokeri2222/KaHack/raw/main/KaHack!.meta.js
-// @downloadURL  https://github.com/jokeri2222/KaHack/raw/main/KaHack!.user.js
-// @author       jokeri2222; https://github.com/jokeri2222
+// @namespace    https://github.com/johnweeky
+// @updateURL    https://github.com/johnweeky/KaHack/raw/main/KaHack!.meta.js
+// @downloadURL  https://github.com/johnweeky/KaHack/raw/main/KaHack!.user.js
+// @author       johnweeky
 // @match        https://kahoot.it/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=kahoot.it
 // @grant        none
